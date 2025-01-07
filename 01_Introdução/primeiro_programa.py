a93243d8db6baa931a267eb0beb6a9e3b7930522
arnaldojrsantos <arnaldojrsantos0@gmail.com>
2025-01-07 16:38:55 -0300

    Update
    Inserindo dados e trabalhando com operador de soma

--- 01_Introdução/primeiro_programa.py
+++ 01_Introdução/primeiro_programa.py
@@ -1,1 +1,10 @@
 print('Hello, World!')
+
+nome = input('Informe seu nome.\n')
+print (f'Olá, {nome}!')
+a = int(input('Digite um número:\n'))
+b = int(input('Digite outro número:\n'))
+
+soma = a + b
+
+print(f'A soma entre {a} e {b} é {soma}.')
