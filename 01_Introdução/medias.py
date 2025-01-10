08fd4d383310f0324be4c74824372053b685f302
arnaldojrsantos <arnaldojrsantos0@gmail.com>
2025-01-10 16:03:15 -0300

    Create medias.py
    Comparação entre média aritmética e média ponderada.

---
+++ 01_Introdução/medias.py
@@ -0,0 +1,19 @@
+print('Vamos fazer a comparação entre média aritimética e ponderada')
+n1 = float(input('Digite a primeia nota.\n'))
+n2 = float(input('Digite a segunda nota.\n'))
+n3 = float(input('Digite a terceira nota.\n'))
+n4 = float(input('Digite a quarta nota.\n'))
+
+mp = (n1*1 + n2 *2 + n3*3 + n4*4)/ (1+2+3+4)
+ma = (n1 + n2 + n3 + n4)/4
+
+if mp == ma:
+    print(f'Sua média PONDERADA é {mp:.2f}. e sua média ARITIMÉTICA é {ma:.2f}\n'
+          'As médias são iguais.')
+
+elif mp > ma:
+    print(f'Sua média PONDERADA é {mp:.2f}. e sua média ARITIMÉTICA é {ma:.2f}\n'
+          f'{mp} é maior que {ma}.')
+else:
+    print(f'Sua média PONDERADA é {mp:.2f}. e sua média ARITIMÉTICA é {ma:.2f}\n'
+          f'{ma} é maior que {mp}.')
